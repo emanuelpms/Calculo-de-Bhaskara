38c71887904666268d8d500efe666f3594e8e27f
emanuelpms <emanuelpms@users.noreply.github.com>
2022-03-20 23:13:37 -0300

    Adicionar arquivos de projeto.

---
+++ Bhaskara/BhaskaraMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bhaskara
+{
+    class BhaskaraMain
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, x1, x2;
+            bool continua = true;
+            char continuar;
+
+            do
+            {
+                Console.WriteLine("============ Calculadora de Bhaskara ================ \n\n\n");
+
+                Console.WriteLine("Digite o valor de A: ");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("========================================================");
+
+                Console.WriteLine("Digite o valor de B: ");
+                b = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("========================================================");
+
+                Console.WriteLine("Digite o valor de C: ");
+                c = Convert.ToDouble(Console.ReadLine());
+
+                CalculoBhaskara.Bhaskara(a, b, c);
+
+                Console.WriteLine("Deseja realizar mais calculos? {S} - Sim {N} - Não");
+                continuar = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (continuar == 'N')
+                {
+                    continua = false;
+                }
+                else
+                {
+                    continua = true;
+                }
+
+
+            } while (continua);
+
+
+        }
+
+        
+        
+
+    }
+}
---
+++ Bhaskara/CalculoBhaskara.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bhaskara
+{
+    class CalculoBhaskara
+    {
+        public static void Bhaskara(double a, double b, double c)
+        {
+            double delta, raiz, x1, x2;
+
+            delta = (b * b) - (4 * a * c);
+            raiz = Math.Sqrt(delta);
+
+            x1 = (-b + raiz) / (2 * a);
+            x2 = (-b - raiz) / (2 * a);
+
+            
+            if (delta < 0)
+            {
+                Console.WriteLine("Esta equação do segundo grau não possui raizes reais ");
+            }
+            else if (delta == 0)
+            {
+                Console.WriteLine("Esta equação do segundo grau possui uma raiz real ");
+                Console.WriteLine($"O valor de X1 = {Math.Round(x1, 0)} e do X2 = {Math.Round(x2, 0)}");
+
+            }
+            else
+            {
+                Console.WriteLine("Esta equação do segundo grau possui duas raizes reais");
+                Console.WriteLine($"O valor de X1 = {Math.Round(x1, 0)} e do X2 = {Math.Round(x2, 0)}");
+            }
+
+        }
+    }
+}
